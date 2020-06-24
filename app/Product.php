6b622528b0da65604f5700cb8b7cdd77df7b4bca
Omar Atta <omaratta212@gmail.com>
2020-06-24 08:09:26 +0200

    Setup product model (#6)
    * Remove frontend files
    
    * Create user seeder & install spatie permissions
    
    * setup user roles & avatar image
    
    * Create prouct~user relations
    
    * Create user & product resources

---
+++ app/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'price', 'main_image', 'details',
+    ];
+
+    /**
+     * @return BelongsTo
+     * @var mixed
+     */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
