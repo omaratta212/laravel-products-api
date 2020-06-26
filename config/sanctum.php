a4b91bd065497c4bdd1bb3d672ffa28101043b7f
Atta <omaratta212@gmail.com>
2020-06-26 15:27:19 +0200

    Add sanctum prefix

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,7 +14,7 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1')),
-
+    'prefix' => 'api/sanctum',
     /*
     |--------------------------------------------------------------------------
     | Expiration Minutes
